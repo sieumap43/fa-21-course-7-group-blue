09df4c8dcbbdaf9aa4d64b44fe5340903e9d5000
Lukas Kamm <lukas.kamm@gmail.com>
2021-09-27 11:58:46 +0200

    Adding comments, console output and simplifications to relabeling script.

--- relabeling_scripts/renameFiles.py
+++ relabeling_scripts/renameFiles.py
@@ -4,38 +4,41 @@ import pandas as pd
 import uuid
 from shutil import copyfile
 
-src = input("Select folder where training data is stored (relative path. Output data will be stored in [SRC]/out): ")
+inputDataStr = input("Select path where driving_log.csv + IMG folder are stored (absolute or relative). Output data will be copied to [SRC]/out/[CAMERA_PERSPECTIVE]): ")
+inputDataPath = Path(inputDataStr)
+print('Searching driving_log.csv inside', inputDataPath)
 
-srcPath = Path(src)
+# camera perspectives
+perspectives = ['left', 'center', 'right']
 
-# create output directories
-destLeft = srcPath / 'out/left'
-destCenter = srcPath / 'out/center'
-destRight = srcPath / 'out/right'
+# create output directories for each camera perspective
+dest = {}
+for p in perspectives:
+	dest[p] = inputDataPath / f'out/{p}' # copy output data to subdirectory out/CAMERA_PERSPECTIVE
+	dest[p].mkdir(parents=True, exist_ok=True) # create subdirectories, skip if already exists
 
-destLeft.mkdir(parents=True, exist_ok=True)
-destCenter.mkdir(parents=True, exist_ok=True)
-destRight.mkdir(parents=True, exist_ok=True)
+df = pd.read_csv(
+	inputDataPath / 'driving_log.csv', # source CSV file
+	delimiter=',', # , as column seperation
+	header=None, # no header row
+	names=['center', 'left', 'right', 'angle', 'acc', 'brake', 'speed'], # column names
+	dtype={'center': str, 'left': str, 'right': str, 'angle': float, 'acc': float, 'break': float, 'speed': float} # data types
+)
 
-df = pd.read_csv(srcPath / 'driving_log.csv', delimiter=',', header=None, names=['center', 'left', 'right', 'angle', 'acc', 'break', 'speed'], dtype={'center': str, 'left': str, 'right': str, 'angle': float, 'acc': float, 'break': float, 'speed': float})
+print('Start copying frames into', inputDataStr, '/out/<CAMERA_PERSPECTIVE>/<Index>_<codedSpeed>_<codedAngle>.jpg')
 copyCount = 0
 for idx, row in df.iterrows():
-	# create new filename: <UUID>_<coded_speed>_<coded_angle>.jpg
-	uuidVal = uuid.uuid1() # Generate a UUID from a host ID, sequence number, and the current time.
+	# create new filename: <Index>_<coded_speed>_<coded_angle>.jpg
+	uuidVal = uuid.uuid1() # Generate a UUID from a host ID, sequence number, and the current time. --> for future use
 	codedSpeed = int(row['speed'] * 1000)
 	codedAngle = int(row['angle'] * 1000)
 	fname = f'{idx}_{codedSpeed}_{codedAngle}.jpg'
-	#print(fname)
-
-	srcLeft = Path(row['left'])
-	srcCenter = Path(row['center'])
-	srcRight = Path(row['right'])
 
 	# Try to copy source files into destination directories, re-labeled
 	try:
-		copyfile(srcLeft, destLeft / fname)
-		copyfile(srcCenter, destCenter / fname)
-		copyfile(srcRight, destRight / fname)
+		for p in perspectives:
+			srcImage = Path(row[p]) # The driving_log.csv file is storing absolute path names --> convert absolute path string to Path object of each source frame
+			copyfile(srcImage, dest[p] / fname)
 		copyCount += 1
 	except Exception as e:
 		continue
